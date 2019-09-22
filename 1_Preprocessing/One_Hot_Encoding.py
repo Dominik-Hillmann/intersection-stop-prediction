d0f4271df0fbb625cfef5e39f0d1f91675f0c44d
Dominik Hillmann <dominik.hillmann@gmx.de>
2019-09-22 15:29:08 +0200

    One-Hot preprocessing script done

--- 1_Preprocessing/One_Hot_Encoding.py
+++ 1_Preprocessing/One_Hot_Encoding.py
@@ -4,6 +4,7 @@ Makes the original dataset train.csv suitable for models by applying one-hot enc
 
 import os
 import sys
+import csv
 
 import pandas as pd
 import numpy as np
@@ -12,15 +13,18 @@ def one_hottify_column(col):
     """Converts a pandas series to an one-hot encoded dataframe for each unique value in the column.
     
     Arguments:
-        col {pandas.Series} -- A pandas series of categorical values,
+        col {pandas.Series} -- A pandas series of categorical values.
+
+    Returns:
+        re_frame {pandas.DataFrame} -- Dataframe with each unique value as column.
     """
 
-    re_frame = pd.DataFrame(index = index)
+    re_frame = pd.DataFrame(index = col.index)
 
     for unique_value in col.unique():
         re_frame.insert(
             len(re_frame.columns),
-            unique_value,
+            col.name + '_' + unique_value,
             (col == unique_value).map({
                 True: 1,
                 False: 0
@@ -30,8 +34,38 @@ def one_hottify_column(col):
     return re_frame
 
 
+def main():
+    train_X = pd.read_csv(os.path.join('..', 'Data', 'train.csv')).set_index('RowId')
+    train_X.drop([
+        'EntryStreetName', 
+        'ExitStreetName', 
+        'Path', 
+        'IntersectionId'
+    ], inplace = True, axis = 1) 
 
-train_X = pd.read_csv(os.path.join('..', 'data', 'train.csv')).set_index('RowId')
-# print(train_X.head(6))
-# print(train_X['City']
-print(one_hottify_column(train_X['City']))
+    categorical_cols = [
+        'EntryHeading', 
+        'ExitHeading',
+        'City'
+    ]
+
+    train_X['idx'] = train_X.index
+    merged = train_X
+    for categorical_col in categorical_cols:
+        merged = pd.merge(
+            left = merged,
+            right = one_hottify_column(merged[categorical_col]),
+            left_index = True,
+            right_index = True
+        )
+        merged.drop(categorical_col, inplace = True, axis = 1)
+
+    merged.to_csv(
+        os.path.join('..', 'Data', 'train_one_hot.csv'),
+        header = True,
+        index = True,
+        quoting = csv.QUOTE_ALL
+    )
+
+if __name__ == '__main__':
+    main()
